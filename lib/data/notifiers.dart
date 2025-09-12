2d94c99e27db491c92919dd691bdfff08a4fc987
JuanS-ryb <juanwot131@gmail.com>
2025-09-11 20:53:55 -0500

    Creacion de themes, aplicados en ciertos elementos de login

--- lib/data/notifiers.dart
+++ lib/data/notifiers.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-ValueNotifier<bool> useDarkTheme = ValueNotifier(false);
+ValueNotifier<bool> useDarkTheme = ValueNotifier(true);
 ValueNotifier<int> renderNotificator = ValueNotifier(0);
 
 // NUEVO: para controlar si el usuario está logeado o no
-ValueNotifier<bool> isLoggedIn = ValueNotifier(true);
+ValueNotifier<bool> isLoggedIn = ValueNotifier(false);
