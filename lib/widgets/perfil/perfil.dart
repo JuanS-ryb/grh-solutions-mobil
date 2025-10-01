ee5fc4357e9aa8901ad8fab837caaea8d05322d1
JuanS <126506142+ryazbo@users.noreply.github.com>
2025-09-30 20:23:11 -0500

    Update perfil.dart

--- lib/widgets/perfil/perfil.dart
+++ lib/widgets/perfil/perfil.dart
@@ -131,7 +131,6 @@ class _PerfilState extends State<Perfil> with TickerProviderStateMixin {
                 ),
                 const Tab(text: "MI INFORMACIÓN"),
                 const Tab(text: "CERTIFICADOS"),
-                const Tab(text: "HOJA DE VIDA"),
               ],
             ),
           ),
